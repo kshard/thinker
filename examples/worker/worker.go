3404add7ea0f537384346a187dc53a03551808f4
fogfish <dmkolesnikov@gmail.com>
2025-03-27 22:36:45 +0200

    encapsulate registry within worker to harden prompts (#10)

---
+++ examples/worker/worker.go
@@ -0,0 +1,65 @@
+//
+// Copyright (C) 2025 Dmitry Kolesnikov
+//
+// This file may be modified and distributed under the terms
+// of the MIT license.  See the LICENSE file for details.
+// https://github.com/kshard/thinker
+//
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/kshard/chatter"
+	"github.com/kshard/chatter/bedrock"
+	"github.com/kshard/thinker/agent"
+	"github.com/kshard/thinker/codec"
+	"github.com/kshard/thinker/command"
+)
+
+func encode(q string) (prompt chatter.Prompt, err error) {
+	prompt.WithTask(`
+		Use available tools to complete the workflow:
+		(1) Create 5 files one by one with few lines of random text, at least one line shall contain "%s".
+		(2) Use available tools to find files containing the string: "%s".
+		(3) Replace the only found string with "XXXXX".
+		(4) Validate completion of task by checking "%s" in the files and fix your self if it still exists.`, q, q, q)
+
+	return
+}
+
+func main() {
+	// enable `shell` command for the agent, the command is sandbox to the dir.
+	registry := command.NewRegistry()
+	registry.Register(command.Bash("MacOS", "/tmp/script"))
+
+	// create instance of LLM client
+	llm, err := bedrock.New(
+		bedrock.WithLLM(bedrock.LLAMA3_1_70B_INSTRUCT),
+		bedrock.WithRegion("us-west-2"),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	// We create an agent that executes the workflow.
+	agt := agent.NewWorker(
+		// enable debug output for LLMs dialog
+		chatter.NewDebugger(os.Stdout, llm),
+
+		// Configures the encoder to transform input of type A into a `chatter.Prompt`.
+		// Here, we use an encoder that builds prompt.
+		codec.FromEncoder(encode),
+
+		// Configure the decoder to transform output of LLM into type B.
+		// Here, we use the tool registry to "decode" output into command call.
+		registry,
+	)
+
+	// Execute agent
+	_, err = agt.Prompt(context.Background(), "Hello World")
+	fmt.Printf("\n\n==> Err: %v", err)
+}
